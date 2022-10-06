ce582e908d20e07379cbf813bff25349747faa60
xXSchrandXx <marvinschrand@yahoo.de>
2022-10-06 18:38:03 +0200

    Fix codestyle

--- files/lib/system/MCSkinPreviewAPI/adapter/ISkinRenderer.class.php
+++ files/lib/system/MCSkinPreviewAPI/adapter/ISkinRenderer.class.php
@@ -6,17 +6,17 @@ namespace wcf\system\MCSkinPreviewAPI\adapter;
  * ISkinRenderer.class.php - Library to render previews of Minecraft (tm) skins
  * Copyright outadoc (https://github.com/outadoc/MC-SkinPreviewAPI)
  * Modified xXSchrandXx
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -150,7 +150,7 @@ interface ISkinRenderer
      *
      * @param \GdImage|\Imagick|resource $img the bitmap to save.
      * @param string $file_path the path of the file.
-     * 
+     *
      * @return bool weather save was successful
      */
     public function writeImage($img, $file_path);
