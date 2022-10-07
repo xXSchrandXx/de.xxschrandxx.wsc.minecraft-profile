d3a9035064fd1711b71200b6cd417e9bf12e45bc
xXSchrandXx <marvinschrand@yahoo.de>
2022-10-07 12:36:44 +0200

    remove sqlSelects

--- files/lib/system/condition/minecraft/MinecraftIDCondition.class.php
+++ files/lib/system/condition/minecraft/MinecraftIDCondition.class.php
@@ -42,8 +42,6 @@ class MinecraftIDCondition extends AbstractMultiSelectCondition implements IObje
             throw new InvalidObjectArgument($objectList, MinecraftProfileList::class, 'Object list');
         }
 
-        $objectList->sqlSelects = 'DISTINCT minecraftUUID';
-
         $objectList->getConditionBuilder()->add(
             $this->fieldName . ' IN (?)',
             [$conditionData[$this->fieldName]]
